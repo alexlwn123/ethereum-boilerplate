870208ee17fd93b0f968d3e85b85e626b653fcba
Alex Lewin <alexlwn123@gmail.com>
2022-03-19 14:18:39 -0400

    added game

--- src/components/QuickStart.jsx
+++ src/components/QuickStart.jsx
@@ -1,8 +1,8 @@
-import { Image } from "antd";
-import React from "react";
+import { Image, Button } from "antd";
+import React, { useState, useEffect } from "react";
 // import { useMoralis } from "react-moralis";
 import Skiing from "../assets/Skiing.jpg";
-// import Unity, { UnityContext } from "react-unity-webgl";
+import Unity, { UnityContext } from "react-unity-webgl";
 
 const styles = {
   title: {
@@ -21,28 +21,45 @@ const styles = {
     marginBottom: "-45px",
   },
 };
-
-// const unityContext = new UnityContext({
-//   loaderUrl: "../../public/snowRider-build.loader.js",
-//   dataUrl: "../../public/snowRider-build.data",
-//   frameworkUrl: "../../public/snowRider-build.framework.js",
-//   codeUrl: "../../public/snowRider-build.wasm",
-// });
-// const unityContext = new UnityContext({
-//   loaderUrl: "public/snowRider-build.loader.js",
-//   dataUrl: "public/snowRider-build.data",
-//   frameworkUrl: "public/snowRider-build.framework.js",
-//   codeUrl: "public/snowRider-build.wasm",
-// });
+const unityContext = new UnityContext({
+  loaderUrl: "/game/snowRider-build.loader.js",
+  dataUrl: "/game/snowRider-build.data",
+  frameworkUrl: "/game/snowRider-build.framework.js",
+  codeUrl: "/game/snowRider-build.wasm",
+});
 
 export default function QuickStart() {
   // const { Moralis } = useMoralis();
   styles.title;
+  const [isLoaded, setIsLoaded] = useState(false);
+  useEffect(() => {
+    unityContext.setFullscreen(true);
+    unityContext.on("loaded", () => {
+      setIsLoaded(true);
+      console.log("loaded!!!");
+    });
+  }, []);
+
+  const handleFullScreen = () => {
+    unityContext.setFullscreen(true);
+  };
 
   return (
     <div style={{ display: "flex", gap: "10px" }}>
-      <Image src={Skiing} />
+      <Image src={Skiing} style={{ width: "0" }} />
+      <Button onClick={() => handleFullScreen()}>FullScreen</Button>
+      <vr />
+      <div style={{ height: 1000 }}>
+        <Unity
+          style={{
+            width: "100%",
+            height: "100%",
+            border: "2px solid black",
+            visibility: isLoaded ? "visible" : "hidden",
+          }}
+          unityContext={unityContext}
+        />
+      </div>
     </div>
-    // <Unity unityContext={unityContext} />
   );
 }
